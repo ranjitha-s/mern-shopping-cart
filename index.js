59b7c8d424d50026eb9a7c3433870068e3ecc35d
ranjitha-s <ranjitha210497@gmail.com>
2020-08-10 10:55:52 +0530

    move private info to dev.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./models/user");
+
+const config = require("./config/key");
+
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error(err));
+
+app.use(bodyParser.urlencoded({ extened: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({ "hello ~": "Hi ~~ sad" });
+});
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userData) => {
+    if (err) return res.json({ success: false, err });
+
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+app.listen(5000);
